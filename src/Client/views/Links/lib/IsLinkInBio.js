af8d7b48b0f6d927f408c86c5eb9142e4daa026b
Billy <billyturpin642@gmail.com>
2022-12-29 02:28:50 +0100

    header

--- src/Client/views/Links/lib/IsLinkInBio.js
+++ src/Client/views/Links/lib/IsLinkInBio.js
@@ -4,10 +4,10 @@ import React from 'react'
 export function IsLinkInBio({Link}) {
   return (
     <>
-        {
-          Link.linkInBio &&
-          <SwatchIcon width={12} className='c-yellow' />
-        }
+      {
+        Link.linkInBio &&
+        <SwatchIcon width={12} className='c-yellow' />
+      }
     </>
   )
 }
