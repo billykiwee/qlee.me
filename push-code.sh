a335980decdef9c74d21d9344df6979525317895
Billy <billyturpin642@gmail.com>
2022-12-31 02:52:28 +0100

    Code pushé automatiquement toutes les 5 minutes

--- push-code.sh
+++ push-code.sh
@@ -3,7 +3,7 @@
 while true; do
  
   # Se déplacer dans le répertoire de votre projet
-  cd /chemin/vers/votre/projet
+  cd https://github.com/billykiwee/loopme.git
 
   # Ajouter tous les fichiers modifiés à l'index
   git add .
