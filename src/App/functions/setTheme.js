aef079c58d0b2469d083c3690af7d07eb5904925
Billy <billyturpin642@gmail.com>
2023-01-01 18:45:09 +0100

    2023-01-01 18:45:09

--- src/App/functions/setTheme.js
+++ src/App/functions/setTheme.js
@@ -1,21 +1,18 @@
-const initialTheme = 'light'
+const initialTheme = 'dark'
 
 const setTheme = theme => {
     localStorage.setItem('theme', theme)
     document.querySelector('body').setAttribute('data-theme', theme)
 }
 
-export const toggleTheme = theme => {
-    setTheme(theme === 'light' ? 'dark' : 'light')
-}
-
+export const toggleTheme = theme => setTheme(theme === 'light' ? 'dark' : 'light')
 
 const savedTheme = localStorage.getItem('theme')
 
 const setThemeOnInit = e => {
     savedTheme 
-    ? document.querySelector('body').setAttribute('data-theme', savedTheme)
-    : setTheme(initialTheme)
+        ? document.querySelector('body').setAttribute('data-theme', savedTheme)
+        : setTheme(initialTheme)
 }
 
 setThemeOnInit()
