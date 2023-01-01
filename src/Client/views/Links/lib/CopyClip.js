ee75dc926e843e3443918dd3733b7a88cfb2f560
Billy <billyturpin642@gmail.com>
2023-01-01 19:24:46 +0100

    2023-01-01 19:24:46

--- src/Client/views/Links/lib/CopyClip.js
+++ src/Client/views/Links/lib/CopyClip.js
@@ -1,24 +1,27 @@
 import { ClipboardIcon } from "@heroicons/react/24/outline"
 
 
-export function copyClipboard(link) {
-    navigator.clipboard.writeText(link.shortLink)
-
-    let div = document.querySelector('#copy-' + link.id)
-
-    div.style.display = 'flex'
-    
-    setTimeout(e=> div.style.display = 'none', 2000)
-}
 
 export function CopyClip(link) {
+
+    console.log(link);
+    
+    function copyClipboard(link) {
+        navigator.clipboard.writeText(link.shortLink)
+    
+        let div = document.querySelector('#copy-' + link.id)
+    
+        div.style.display = 'flex'
+        
+        setTimeout(e=> div.style.display = 'none', 2000)
+    }
 
     return (
         <div className='display'>
             <div className='display justify-c border-r-04 w-2 hover h-2 click' onClick={e=> copyClipboard(link)} >
                 <ClipboardIcon width={18} className='c-black' />
             </div>
-            <div className='display disable green absolute border-r-04 p-04' id={'copy-' + link.id} >
+            <div className='display disable green absolute border-r-04 p-04' id={'copy-' +link.id} >
                 <small>Copié</small>
             </div>
         </div>
