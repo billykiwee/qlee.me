ea41014fb390ca2a5d5a0df365869fc1935ffef1
Billy <billyturpin642@gmail.com>
2023-01-02 14:05:12 +0100

    2023-01-02 14:05:12

---
+++ css.js
@@ -0,0 +1,22 @@
+export const css = [
+    {
+        name: 'w-',
+        value: 'width'
+    },
+    {
+        name: 'h-',
+        value: 'width'
+    },
+    {
+        name: 'mt-',
+        value: 'width'
+    },
+    {
+        name: 'mb-',
+        value: 'width'
+    },
+    {
+        name: 'p-',
+        value: 'width'
+    },
+]
