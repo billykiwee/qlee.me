8b5d5fab19a604ffbac55290790a5973324cf622
Billy <billyturpin642@gmail.com>
2023-01-11 11:06:06 +0100

    2023-01-11 11:06:06

--- src/Client/views/Stats/data/dataFilters.js
+++ src/Client/views/Stats/data/dataFilters.js
@@ -3,45 +3,46 @@ import { countBy } from '../functions/countBy'
 export const dataFilter = (LinkStat) => {
 
     if (!LinkStat) return 
-    
-    const views = LinkStat.length
-
-    const device = LinkStat.map(e=> e.device)
-    const countByDevice = countBy(device)
-
-    const reference = LinkStat.filter(e=> e.reference !== '').map(e=> e.reference && new URL(e.reference).origin)
-    const countByReference = countBy(reference)
-
-    const countries = LinkStat.map(e=> e.adress?.country_code + '__'+ e.adress?.country)
-    const countByCountry = countBy(countries)
-
-    const performance = LinkStat.map(e=> e.performance)
-    const countPerformance = performance.length && ((performance.reduce((x,y) => x + y) / performance.length / 1000)).toFixed(2) + 's'
-
 
     const data = {
-        clics :  LinkStat.length,
+        clics : LinkStat.length,
         device: {
             filter: LinkStat.map(e=> e.device),
-            count : () => countBy(data.device)
+            count : () => countBy(data.device.filter)
         },
         reference: {
             filter: LinkStat.filter(e=> e.reference !== '').map(e=> e.reference && new URL(e.reference).origin),
-            count : () => countBy(data.reference)
+            count : () => countBy(data.reference.filter)
+        },
+        countries: {
+            filter: LinkStat.map(e=> e.adress?.country_code + '__'+ e.adress?.country),
+            count : () => countBy(data.countries.filter)
+        },
+        performance: {
+            filter: LinkStat.map(e=> e.performance),
+            count : () => {
+                if (data.performance.filter.length) {
+                    let lenght       = data.performance.filter.length
+                    let sort         = data.performance.filter.reduce((x,y) => x + y)
+                    let result       = (sort / lenght) / 1000
+                    let resultSecond = result.toFixed(2) + 's'
+                    return resultSecond
+                }
+            }
         },
     }
 
-   console.log( data.count);
+    const { clics, device, reference, countries, performance } = data
 
 
     return {
-        clics       : views,
-        device      : Object.values(countByDevice),
-        reference   : Object.values(countByReference),
-        localisation: Object.values(countByCountry),
+        clics       : clics,
+        device      : device.count(),
+        reference   : reference.count(),
+        localisation: countries.count(),
         performance : { 
-            performance, 
-            speed: countPerformance
+            performance: performance.filter,
+            speed      : performance.count()
         }
     } 
 
