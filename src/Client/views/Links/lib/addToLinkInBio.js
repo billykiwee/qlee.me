38cdb6e616b84d0baf6818cf9d866720be1c485e
Billy <billyturpin642@gmail.com>
2023-01-12 15:47:08 +0100

    2023-01-12 15:47:08

--- src/Client/views/Links/lib/addToLinkInBio.js
+++ src/Client/views/Links/lib/addToLinkInBio.js
@@ -1,16 +1,21 @@
 import { db } from "../../../../App/database/firebase"
 
+
 export const addToLinkInBio = (isCheck, LinkID, user, linkData) => {
+
+    const ref = db.collection('link-in-bio')
+    .doc(user.link_in_bio)
+    .collection('links')
 
     db
     .collection('links')
     .doc(LinkID)
     .update({ linkInBio: isCheck })
 
-    db
-    .collection('link-in-bio')
-    .doc('bill')
-    .set({
-        ...linkData
-    })
+    if (isCheck) {
+        ref.doc(LinkID).set(linkData.filter(l=> { l.id, l.url, l.shortLink, l.icon, l.name }))
+    }
+    else {
+        ref.doc(LinkID).delete()
+    }
 }
