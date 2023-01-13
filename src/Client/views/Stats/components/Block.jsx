2a1270ad422f8dc0e1806c57aa813f46a85af263
Billy <billyturpin642@gmail.com>
2023-01-13 04:02:15 +0100

    2023-01-13 04:02:15

--- src/Client/views/Stats/components/Block.jsx
+++ src/Client/views/Stats/components/Block.jsx
@@ -4,8 +4,6 @@ import getFavicon from '../../../../App/utils/getFavicon'
 import { isValidUrl } from '../../../../App/utils/isValidUrl'
 import { getHostName } from '../../../lib/getHostName'
 import { GoToPricing } from '../../Links/views/Edit/Edit'
-import getUnicodeFlagIcon from 'country-flag-icons/unicode'
-import { DevicePhoneMobileIcon, EyeIcon, GlobeEuropeAfricaIcon, MapPinIcon, RocketLaunchIcon } from '@heroicons/react/24/solid'
 import { ProgressBar } from './ProgressBar'
 
 
@@ -24,7 +22,7 @@ export const Block = ({ User, statistic }) => {
     return (
         <div className='grid gap-1rem grey p-1 border-r-04'>
         
-            <div className={isUserPremium(User).plan !== 'ENTREPRISE' ? 'display justify-s-b' : 'grid gap-1rem'} >
+            <div className={isUserPremium(User).plan !== 'ENTREPRISE' || type === 'clics' ? 'display justify-s-b' : 'grid gap-1rem'} >
                 <div className='display gap' >
                     {icon}
                     <span>{title}</span>
@@ -35,22 +33,25 @@ export const Block = ({ User, statistic }) => {
                         .sort((x, y)=> y.count - x.count)
                         .map(stat=> {
 
-                            console.log(stat);
+                            if (!Object.values(stat)[1]) return <small className='c-grey'>Aucune données</small>
 
                             if (type === 'clics') {
                                 return (
                                     <div className='display justify-s-b '>
-                                        <span>{stat.name}</span> 
                                         <span>{stat.count}</span>
                                     </div>
                                 )
                             }
 
+                            if (isUserPremium(User).plan !== 'ENTREPRISE') return <GoToPricing />
+
                             if (type == 'device') {
+
+                               if (!Object.values(stat.array).length) return <small className='c-grey'>Aucune données</small>
+                                
                                 return Object.values(stat.array)
                                 .sort((x, y)=> y.count - x.count)
-                                .map(array=> {
-                                    
+                                .map(array=> {                                    
                                     return (
                                         <div className='display justify-s-b'>
                                             <div className='display gap'>
@@ -63,41 +64,49 @@ export const Block = ({ User, statistic }) => {
                                 })
                             }
 
-                            if (type == 'reference')
-                            return Object.values(stat.array)
-                            .sort((x, y)=> y.count - x.count)
-                            .map(array=> {
+                            if (type == 'reference') {
 
-                                return (
-                                    <div className='display justify-s-b'>
-                                        <div className='display gap'>
-                                            <img src={getFavicon(array.name)} width={16} className='border-r-2' />
-                                            {
-                                                isValidUrl(array.name) 
-                                                ? <span>{getHostName(array.name)}</span> 
-                                                : <span>autres</span>
-                                            }
-                                            <small className='c-grey f-s-12'>{array.count}</small>
-                                        </div>
-                                        <ProgressBar percentage={percentage(stat, array)} />
-                                    </div>
-                                )
-                            })
+                                if (!Object.values(stat.array).length) return <small className='c-grey'>Aucune données</small>
 
-                           if (type == 'localisation')
-                           return Object.values(stat.array)
-                           .sort((x, y)=> y.count - x.count)
-                           .map(array=> {
-                                return (
-                                    <div className='display justify-s-b'>
-                                        <div className='display gap'>
-                                            <span>{array.name.split('__')[1]}</span> 
-                                            <small className='c-grey f-s-12'>{array.count}</small>
+                                return Object.values(stat.array)
+                                .sort((x, y)=> y.count - x.count)
+                                .map(array=> {
+    
+                                    return (
+                                        <div className='display justify-s-b'>
+                                            <div className='display gap'>
+                                                <img src={getFavicon(array.name)} width={16} className='border-r-2' />
+                                                {
+                                                    isValidUrl(array.name) 
+                                                    ? <span>{getHostName(array.name)}</span> 
+                                                    : <span>autres</span>
+                                                }
+                                                <small className='c-grey f-s-12'>{array.count}</small>
+                                            </div>
+                                            <ProgressBar percentage={percentage(stat, array)} />
                                         </div>
-                                        <ProgressBar percentage={percentage(stat, array)} />
-                                    </div>
-                                )
-                           })
+                                    )
+                                })
+                            }
+
+                           if (type == 'localisation') {
+
+                                if (!Object.values(stat.array).length) return <small className='c-grey'>Aucune données</small>
+                          
+                                return Object.values(stat.array)
+                                .sort((x, y)=> y.count - x.count)
+                                .map(array=> {
+                                     return (
+                                         <div className='display justify-s-b'>
+                                             <div className='display gap'>
+                                                 <span>{array.name.split('__')[1]}</span> 
+                                                 <small className='c-grey f-s-12'>{array.count}</small>
+                                             </div>
+                                             <ProgressBar percentage={percentage(stat, array)} />
+                                         </div>
+                                     )
+                                })
+                           }
                             if (type == 'performance')
                             return (
                                 <div className='display justify-s-b'>
