6fda7aca75790d448c0f9a393629672bd32e2711
Billy <billyturpin642@gmail.com>
2023-01-17 17:53:24 +0100

    2023-01-17 17:53:24

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 import Home from '../Website/views/Home/Home'
 
