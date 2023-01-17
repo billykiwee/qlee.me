0273c7547deed941881f0ab4bdb92bbdf083ec3a
Billy <billyturpin642@gmail.com>
2023-01-17 18:50:21 +0100

    2023-01-17 18:50:21

--- src/App/App.js
+++ src/App/App.js
@@ -1,48 +1,15 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
-import Home from '../Website/views/Home/Home'
-
-import Dashboard from '../Client/views/Dashboard/Dashboard'
-import Edit from '../Client/views/Links/views/Edit/Edit'
 import Header from './components/header/Header'
-
 import Footer from './components/Footer'
-import Login from '../Website/views/Login/Login'
-import Page404 from '../Website/views/Page404'
-import Pricing from '../Website/views/Pricing/Pricing'
-import Stats from '../Client/views/Stats/Stats'
-import Redirection from '../Client/views/Redirection/Redirection.jsx'
-import Payment from '../Website/views/Payment/Payment'
-import LinkInBio from '../Client/views/LinkInBio/LinkInBio'
-import Profil from '../Client/views/Profil/Profil'
-import Terms from '../Website/views/Terms/Terms'
-import { EditLinkInBio } from '../Client/views/LinkInBio/views/Edit/Edit'
+
 import { SnackBar } from './components/snackBar/SnackBar'
 import Popup from './components/popUp/Popup'
+import { router } from './router/router'
 
 
 export default function App() {
-
-    const router = [
-        { path : '/*', element : <Page404 /> },
-        { path : '/page404', element : <Page404 /> },
-        { path : '/', element : <Home /> },
-        { path : '/dashboard', element : <Dashboard /> },
-        { path : '/edit/:LinkID', element : <Edit /> },
-        { path : '/:LinkID', element : <Redirection /> , blank : true},
-        { path : '/login', element : <Login /> },
-        { path : '/pricing', element : <Pricing /> },
-        { path : '/stats', element : <Stats /> },
-        { path : '/stats/:LinkID', element : <Stats /> },
-        { path : '/payment/:planID', element : <Payment /> },
-        { path : '/@:userName', element : <LinkInBio /> , blank : true},
-        { path : '/edit/@:userName', element : <EditLinkInBio /> },
-        { path : '/profil', element : <Profil /> },
-        { path : '/terms', element : <Terms /> },
-    ]
-
-
 
     return (
         <BrowserRouter>
---
+++ src/App/router/router.js
@@ -0,0 +1,31 @@
+import Dashboard from "../../Client/views/Dashboard/Dashboard";
+import LinkInBio from "../../Client/views/LinkInBio/LinkInBio";
+import { EditLinkInBio } from "../../Client/views/LinkInBio/views/Edit/Edit";
+import Edit from "../../Client/views/Links/views/Edit/Edit";
+import Profil from "../../Client/views/Profil/Profil";
+import Redirection from "../../Client/views/Redirection/Redirection";
+import Stats from "../../Client/views/Stats/Stats";
+import Home from "../../Website/views/Home/Home";
+import Login from "../../Website/views/Login/Login";
+import Page404 from "../../Website/views/Page404";
+import Payment from "../../Website/views/Payment/Payment";
+import Pricing from "../../Website/views/Pricing/Pricing";
+import Terms from "../../Website/views/Terms/Terms";
+
+export const router = [
+    { path : '/*', element : <Page404 /> },
+    { path : '/page404', element : <Page404 /> },
+    { path : '/', element : <Home /> },
+    { path : '/dashboard', element : <Dashboard /> },
+    { path : '/edit/:LinkID', element : <Edit /> },
+    { path : '/login', element : <Login /> },
+    { path : '/pricing', element : <Pricing /> },
+    { path : '/stats', element : <Stats /> },
+    { path : '/stats/:LinkID', element : <Stats /> },
+    { path : '/payment/:planID', element : <Payment /> },
+    { path : '/:LinkID', element : <Redirection /> , blank : true},
+    { path : '/@:userName', element : <LinkInBio /> , blank : true},
+    { path : '/edit/@:userName', element : <EditLinkInBio /> },
+    { path : '/profil', element : <Profil /> },
+    { path : '/terms', element : <Terms /> },
+]
