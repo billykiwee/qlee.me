cedd33d9f5634ee92031bcccbdf60ec78fbf99a5
Billy <billyturpin642@gmail.com>
2023-01-27 09:46:50 +0100

    2023-01-27 09:46:50

--- src/Website/views/Features/Features.jsx
+++ src/Website/views/Features/Features.jsx
@@ -1,26 +1,28 @@
 import { ChartPieIcon, Cog6ToothIcon, PencilIcon, QrCodeIcon } from '@heroicons/react/24/solid'
 import React from 'react'
 import Main from '../../../App/components/Main'
+import { GetWidth } from '../../../App/utils/GetWidth'
 
 export default function Features() {
+
+
+    const width = GetWidth()
 
     return (
         <Main>
             <h2 className='m-0'>Fonctionnalités</h2>
 
-            <div className='display justify-s-b'>
+            <div className='display justify-c'>
 
-                <div className='grid gap m-t-2 w-100p'>
-                    <div className='grid gap p-1' >
+                <div className='grid gap m-t-2' style={{ width : width > 780 ? '80%' : '100%' }}>
+                    <div className='grid gap p-1 border white p-1 border-r-1' >
                         <div className='display gap-1rem'>
-                            <span className='border-r-100 w-3 h-3 display justify-c white'><PencilIcon width={18} className='c-blue' /></span>
                             <span className='f-s-25'>Créer</span>
                         </div>
                     </div>
 
                     <div className='grid gap p-1' >
                         <div className='display gap-1rem'>
-                            <span className='border-r-100 w-3 h-3 display justify-c white'><Cog6ToothIcon width={20} className='c-blue' /></span>
                             <span className='f-s-25'>Personaliser</span>
                         </div>
                     </div>
@@ -28,14 +30,12 @@ export default function Features() {
 
                     <div className='grid gap p-1' >
                         <div className='display gap-1rem'>
-                            <span className='border-r-100 w-3 h-3 display justify-c white'><ChartPieIcon width={20} className='c-blue' /></span>
                             <span className='f-s-25'>Statistiques</span>
                         </div>
                     </div>
 
                     <div className='grid gap p-1' >
                         <div className='display gap-1rem'>
-                            <span className='border-r-100 w-3 h-3 display justify-c white'><QrCodeIcon width={20} className='c-blue' /></span>
                             <span className='f-s-25'>Qr code</span>
                         </div>
                     </div>
