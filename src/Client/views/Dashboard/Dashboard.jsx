4d0d06bf452b5fcbb4db385d516e43b261a07f0b
Billy <billyturpin642@gmail.com>
2023-01-30 12:51:44 +0100

    2023-01-30 12:51:44

--- src/Client/views/Dashboard/Dashboard.jsx
+++ src/Client/views/Dashboard/Dashboard.jsx
@@ -34,6 +34,14 @@ export default function Dashboard() {
                             <Articles links={UserLinks} user={User} />
                         </div> */}
 
+
+                        <div>
+                            <div >
+                                <img src='http://www.arth-paysage.com/01_Contenus/Articles/elagage//elagage-montage-2.jpg' />
+                            </div>
+                        </div>
+                        
+
                         <form className='grid gap-2rem ' 
                             onSubmit={e=> {
                                 e.preventDefault()
