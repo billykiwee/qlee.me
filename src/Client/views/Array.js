1355e98c5fcd9061366cb5520dd7e25e2cad8978
Billy <billyturpin642@gmail.com>
2023-02-18 22:26:17 +0100

    2023-02-18 22:26:17

--- src/Client/views/Array.js
+++ src/Client/views/Array.js
@@ -5,9 +5,15 @@ export function ArrayLesson() {
 
 
 
-    const array = [1,5,10,3,6]
+const array = [1,5,10,3,6]
 
 
+const sumArray = array.reduce((acc, currentV)=>{
+    return acc + currentV
+})
+
+console.log(sumArray) 
+// 25 qui est la somme des valeurs du tableau 
 
 
 }
