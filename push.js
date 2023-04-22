cfb8545fc2d6f8473276f73a3e1ed7f69f7e0a39
Billy <billyturpin642@gmail.com>
2023-04-23 00:43:03 +0200

    Sun Apr 23 2023 00 h 43 min 03 s

---
+++ push.js
@@ -0,0 +1,26 @@
+const { exec } = require("child_process");
+const express = require("express");
+
+const app = express();
+
+const pushEveryXMinutes = 1;
+
+function gitPush() {
+  const date =
+    new Date().toDateString() + " " + new Date().toLocaleTimeString();
+  exec(
+    `git add . && git commit -m "${date}" && git push`,
+    (err, stdout, stderr) => {
+      console.log(
+        "\x1b[32m%s\x1b[0m",
+        `Les modifications ont été poussées à ${date}`
+      );
+    }
+  );
+}
+
+app.listen(8080, () => {
+  gitPush();
+  setInterval(gitPush, pushEveryXMinutes * 1000 * 60);
+  console.log("push running");
+});
