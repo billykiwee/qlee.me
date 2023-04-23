b3065f52cda6c3656886dbe8ad1a5d5db1b1545c
Billy <billyturpin642@gmail.com>
2023-04-23 14:41:31 +0200

    push script v2

--- push.js
+++ push.js
@@ -4,37 +4,48 @@ const app = express();
 
 const args = process.argv.slice(2);
 
-const name = args.filter((a) => a !== "-m").join(" ");
-
-const loop = {
-  active: args.includes("-loop"),
-  time: args.splice(args.length, args.length - 1),
+const cmds = {
+  loop: "-l",
+  message: "-m",
 };
-console.log(loop);
+
+const CLI = {
+  message: args
+    .splice(args.indexOf(cmds.message) + 1, args.indexOf(cmds.loop) - 1)
+    .join(" "),
+  loop: {
+    active: args.includes(cmds.loop),
+    time: Number(
+      args.splice(args.indexOf(cmds.loop), args.indexOf(cmds.loop) + 1)[1]
+    ),
+  },
+  date: new Date().toLocaleDateString() + " at " + new Date().toTimeString(),
+};
+console.log(args.indexOf("-m" | "-mes"));
 
 function gitPush() {
-  const date =
-    new Date().toLocaleDateString() + " at " + new Date().toTimeString();
-
-  exec(
-    `git add . && git commit -m "${name}" && git push`,
-    (err, stdout, stderr) => {
-      if (err) {
-        console.log(stdout);
-        return;
-      }
-      console.log("\x1b[32m%s\x1b[0m", `Code pushed`);
-      console.log(`   message : ${name}`);
-      console.log(`   date : ${date}`);
+  //const cmd = `git add . && git commit -m "${name}" && git push`;
+  const cmd = "lool";
+  exec(cmd, (err, stdout, stderr) => {
+    if (err) {
+      console.log(stdout);
+      return;
     }
-  );
+    console.log("\x1b[32m%s\x1b[0m", `Code pushed`);
+    console.log(`   message : ${CLI.message}`);
+    console.log(`   date : ${CLI.date}`);
+  });
 }
 
 app.listen(8080, () => {
-  gitPush();
-
-  /* if (loop) {
+  /* if (loop.active) {
     setInterval(gitPush, loop.time * 1000 * 60);
   } */
+
+  exec("lol :", (err, stdout, stderr) => {
+    console.log(stderr);
+  });
+  gitPush();
+
   console.log("push running");
 });
