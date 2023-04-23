58e79fd0123c4e03083e950f564985f8c60c9c92
Billy <billyturpin642@gmail.com>
2023-04-23 13:46:33 +0200

    push

--- push.js
+++ push.js
@@ -5,18 +5,22 @@ const app = express();
 
 const pushEveryXMinutes = 10;
 
+const args = process.argv.slice(2);
+const name = args[0];
+
 function gitPush() {
   const date =
     new Date().toDateString() + " " + new Date().toLocaleTimeString();
+
   exec(
-    `git add . && git commit -m "${date}" && git push`,
+    `git add . && git commit -m "${name}" && git push`,
     (err, stdout, stderr) => {
       if (err) {
         return;
       }
       console.log(
         "\x1b[32m%s\x1b[0m",
-        `Les modifications ont été poussées à ${date}`
+        `Les modifications ont été poussées à ${name}`
       );
     }
   );
